faa89eca847cca6e70f96ce63285e6bcbc8abca4
Baptiste Nuytten <baptistenuytten@gmail.com>
2025-12-08 16:41:23 +0100

    Add testFile

---
+++ TestSetup.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class TestSetup : Node
+{
+    public override void _Ready()
+    {
+        // Ce message apparaîtra en bas de ton écran Godot
+        GD.Print("VICTOIRE ! Rider et Godot communiquent parfaitement !");
+    }
+}
